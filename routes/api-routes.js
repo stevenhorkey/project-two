cc514fed7b99c5ec3a855a4b8b3246922c88c829
stevenhorkey <stevenhorkey@gmail.com>
2018-05-06 13:18:25 -0700

    test

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,11 @@
 var db = require("../models");
-
+var paddId = require('../config/passport/passport')
 
 module.exports = function (app) {
     console.log(db)
     console.log(db.Goal)
     app.post("/api/goals", function(req, res) {
-        
+        req.body.userId = passId.userId;
         db.Goal.create(req.body).then(function(dbGoal) {
             res.json(dbGoal);
         });
