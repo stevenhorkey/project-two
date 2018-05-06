1666ea057d3b2fea3dcf96ddc805fa12a1d85bc6
stevenhorkey <stevenhorkey@gmail.com>
2018-05-06 12:09:28 -0700

    changed theme color

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,11 @@
+var db = require("../models");
+
 module.exports = function (app) {
+
+    app.post("/api/goals", function(req, res) {
+        db.Goal.create(req.body).then(function(dbGoal) {
+            res.json(dbGoal);
+        });
+    });
 
 };
