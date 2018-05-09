54d67b55679eff2fce98065949f2b516457b1fac
Cheddar Kregg <cheddarkregg@Cheddars-MacBook-Pro.local>
2018-05-08 18:23:04 -0700

    idk

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -1,28 +1,30 @@
-(function() {
+(function () {
     'use strict';
-    window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-        }, false);
-    });
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
     }, false);
 })();
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    $('#sign-up-btn').on('click',function(event){
+    var userId;
+
+    $('#sign-up-btn').on('click', function (event) {
         event.preventDefault();
     });
 
-    var userId;
-    $.getJSON("api/user_data", function(data) {
+
+    $.getJSON("api/user_data", function (data) {
         // Make sure the data contains the username as expected before using it
         if (data.hasOwnProperty('userId')) {
             console.log(data);
@@ -31,41 +33,42 @@ $( document ).ready(function() {
         }
     });
 
-    $('#goal-submit').on('click',function(event){
+    $('#goal-submit').on('click', function (event) {
+
         event.preventDefault();
-        console.log('add clicked')
+        console.log('add clicked', userId)
         var newGoal = {
-            UserId : userId,
+            UserId: userId,
             goal_name: $('#goal-name-input').val().trim(),
             goal_description: $('#goal-description-input').val().trim(),
-            completed : false
+            completed: false
         }
-        $.ajax('/api/goals',{
-        type:'POST',
-        data: newGoal
+        $.ajax('/api/goals', {
+            type: 'POST',
+            data: newGoal
         }).then(
-        function(){
-        console.log('Created new goal');
-        location.reload();
-        });
+            function () {
+                console.log('Created new goal');
+                location.reload();
+            });
     })
-    $('.my-goals-list').on('click',function(event){
+    $('.my-goals-list').on('click', function (event) {
         event.preventDefault();
         var id = $(this).data('id');
 
-        console.log('Goal clicked: '+id)
+        console.log('Goal clicked: ' + id)
         var toggleBool = {
-            completed : true
+            completed: true
         }
-        $.ajax('/api/goals/'+id,{
-            type:'PUT',
+        $.ajax('/api/goals/' + id, {
+            type: 'PUT',
             data: toggleBool
         }).then(
-        function(){
-        location.reload();
-        });
+            function () {
+                location.reload();
+            });
     })
-    $('#del-completed-goals').on('click',function(event){
+    $('#del-completed-goals').on('click', function (event) {
         event.preventDefault();
 
         var goalsUserId = {
@@ -73,24 +76,24 @@ $( document ).ready(function() {
         }
 
         console.log('Goal deleted')
-        $.ajax('/api/goals/',{
-            type:'DELETE',
+        $.ajax('/api/goals/', {
+            type: 'DELETE',
             data: goalsUserId
         }).then(
-        function(){
-            location.reload();
-        });
+            function () {
+                location.reload();
+            });
     })
-    $('.fa-trash-alt').on('click',function(event){
+    $('.fa-trash-alt').on('click', function (event) {
         event.preventDefault();
         var id = $(this).data('id');
-        console.log('Goal deleted:'+id)
-        $.ajax('/api/goals/'+id,{
-            type:'DELETE',
+        console.log('Goal deleted:' + id)
+        $.ajax('/api/goals/' + id, {
+            type: 'DELETE',
         }).then(
-        function(){
-            // location.reload();
-        });
+            function () {
+                // location.reload();
+            });
     })
 
     module.exports = {
