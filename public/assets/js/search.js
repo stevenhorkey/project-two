d18c175192b27c5306257291a83e5bcd0a447856
CNCodes <colindavid.nelson@gmail.com>
2018-05-09 16:51:15 -0700

    test

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,22 +1,39 @@
+//executes event listeners on page load
 $(document).ready(function () {
+    //declares an empty variable
     let searchName;
-
-    $("#search").on("click", e => {
+    //this on click function controls the search bar for finding other users
+    $("#searchButton").on("click", e => {
+        //prevents page from loading on submit
         e.preventDefault();
-
+        //gets the user-inputted search and assigns it to a variable
         searchName = $("#search-input").val().trim();
+        //console.logs for testing
         console.log(searchName);
+        //actual ajax get request. searchname is included in url for later use
+        $.ajax('/search/' + searchName, {
+            type: 'GET'
+        }).then(function (data) {
+            //alert user of success
+            console.log(data);
+            console.log("You Pushed the Search Button");
+            //relocate them to a new page based off their search
+            window.location.replace('/search/' + searchName)
+        })
+    });
 
-        window.location.replace('/search/' + searchName)
-        // $.ajax('/search/' + searchName, {
-        //     type: 'GET'
-        // }).then(function (data) {
-        //     console.log(data);
-        //     console.log("You Pushed the Search Button");
-        // })
-
-
-
-
+    //this on click function controls the user selecting another users profile to view
+    $(".btn-search-user").on("click", function(){
+        //gets id of the slected users profile to view
+        var userId = $(this).data('id');
+        //actual ajax get request userId is in the url for later use
+        $.ajax('/profile/' + userId, {
+            type: 'GET'
+        }).then(function(data){
+            //alert user of success
+            console.log(data);
+            console.log('You pressed a User Profile Button!');
+        })
     })
-})
+
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,64 +14,105 @@ function isLoggedIn(req, res, next) {
 }
 
 module.exports = function (app) {
-
+    //This handles the get request for the current users profile page
     app.get("/profile", isLoggedIn, function (req, res) {
         console.log(req.user);
+        //sequelize function to findall of goals in database where conditions are met
         db.Goal.findAll({
             where: {
+                //only provides users who belong to current sessions user
+                //passport sends session data in every request as req.user
                 UserId: req.user.id
             }
         }).then(function (dbGoal) {
+            //Object to send to the handlebars file
             var hbObject = {
+                //has all matching goals from search
                 goals: dbGoal,
+                //current session's user
                 users: req.user
             };
+            //console.log for test
             console.log(hbObject.goals)
+            //renders handlebars profile page and gives hbObject to file to handlebars to generate goals and user info
             res.render("profile", hbObject);
         });
 
     });
-
-
-    app.get("/search/:name", (req, res) => {
+    //this handles the get requests for searches, the :name is provided on the client side search.js file
+    app.get("/search/:name", isLoggedIn, (req, res) => {
+        console.log("recieved request")
+        //sequelize function to find all users that match params
         db.User.findAll({
             where: {
+                //only finds users that have a first name of whatever name was searched
                 firstName: req.params.name
             }
         }).then(dbUser => {
+            //provides an object to send to handlebars with the searched users data
             let hbObject = {
                 users: dbUser
             };
+            //load the search handlebars file and pass it the hbObject to provide a list of matched users
             res.render("search", hbObject);
         })
     })
-
+    //this function handles the users request to view a different user profile  
+    app.get("profile/:id", isLoggedIn, function(req, res) {
+        //console.log to confirm the request has been sent
+        console.log("recieved request for profile/" + req.params.id);
+        //sequelize function to find the one matching user based off of user id
+        db.User.findOne({
+            //this is the condition for the user
+            where: {
+                id = req.params.id
+            }
+        }).then(function(dbUser){
+            //hbObject is given a key value pair for the desired profile's user's data
+            var hbObject = {
+                users: dbUser
+            };
+            //render the visitProfile handlebars page sending the hbObject to find
+            res.render("visitProfile", hbObject)
+        })
+    })
+    //this function handles the request to create a new goal
     app.post("/api/goals", function (req, res) {
+        //req.body is equal to the data we sent in the ajax call on search.js
         var newGoal = req.body;
+        //creating a new key called UserId in the object and giving that key a value of the session users id in the database
         newGoal['UserId'] = req.user.id
+        //creates a new goal in the database
         db.Goal.create(newGoal).then(function (dbGoal) {
             res.json(dbGoal);
         });
     });
+    //this function handles the request to change a goal to completed
     app.put("/api/goals/:id", function (req, res) {
-
+        //updates the Goals table with completed: true
         db.Goal.update(
+            //req body matches the toggleBool object from site.js
             req.body,
             {
+                //condition for the matching goal
                 where: {
                     id: req.params.id
                 }
             }
         ).then(function (dbGoal) {
+            //return json of the goal
             res.json(dbGoal);
         });
     });
+    //this function is for handling the request to change a profile picture img src url
     app.put("/api/profile", function (req, res) {
 
         db.User.update(
+            //this is the new profile url
             req.body,
             {
                 where: {
+                    //specific user primary key id
                     id: req.user.id
                 }
             }
@@ -79,11 +120,14 @@ module.exports = function (app) {
             res.json(dbUser);
         });
     });
+    //this handles deleting all of the completer goals
     app.delete("/api/goals", function(req, res) {
         console.log(req.user.id);
         db.Goal.destroy({
             where: {
+                //req.user.id is the current sessions user id
                 UserId: req.user.id,
+                //completed is a boolean value in the users table
                 completed: true
             }
         }).then(function (dbGoal) {
