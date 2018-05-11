d958996a5b80c7325c3f64ba3fd34b96269c10d8
Cheddar Kregg <cheddarkregg@Cheddars-MacBook-Pro.local>
2018-05-10 19:05:41 -0700

    cant go back to logged in screen when logged in

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require('./routes/auth-routes')(app);
 
 require('./config/passport/passport.js')(passport, db.User);
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync({}).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
